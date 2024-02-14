ea098e365f40a1c4b62b704604bf9bbe28522119
ignaarguello <igna.arguello2110@hotmail.com>
2024-02-14 00:06:38 -0300

    Fix:app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { InformesModule } from './informes/informes.module';
 import { MongooseModule } from '@nestjs/mongoose'
 
+
 import { ConfigModule } from '@nestjs/config'
 
 @Module({
